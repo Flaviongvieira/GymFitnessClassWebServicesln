895aa9a755bcbaf616da9e9ab9113b3afd39f0b5
flavio <flaviongvieira@gmail.com>
2022-12-08 13:58:20 +0000

    GymRepository added

---
+++ GymRepository/IGymRepo.cs
@@ -0,0 +1,61 @@
+﻿using GymModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRepository
+{
+    public interface IGymRepo
+    {
+        /*********************** Fitness Instructor ***********************/
+        // create new instructor
+        public void AddInstructor(FitnessInstructor fitinstr);
+
+        // get list of all instructors
+        public IEnumerable<FitnessInstructor> GetInstructors();
+
+        // get instructor by name
+        public IEnumerable<FitnessInstructor> GetAllInstructorsbyName(string instrname);
+
+        // get instructor by id
+        public FitnessInstructor GetInstructorbyId(int id);
+
+        /*********************** Fitness Studio ***********************/
+        // create new studio
+        public void AddStudio(FitnessStudio fitstudio);
+
+        // get list of all sutdios
+        public IEnumerable<FitnessStudio> GetStudios();
+
+        // get studio by name
+        public IEnumerable<FitnessStudio> GetStudiosbyName(string sutdname);
+
+        // get studio by id
+        public FitnessStudio GetStudiobyId(int id);
+
+        /*********************** Fitness Classes ***********************/
+        // create Fitness Class
+        public void AddFitClass(FitnessClassSchedule fitclass);
+
+        // editing existing class
+        public void EditFitClass(int id, FitnessClassSchedule fitclass);
+
+        // delete existing class
+        public void DeleteFitClass(int id);
+
+        // get all classes order by weekday, starttime
+        public IEnumerable<FitnessClassSchedule> GetFitClassSchedules();
+
+        // get fitness classes for a specific day
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByDay(DayOfWeek day);
+
+        // get fitness classes for a specific fitness instructor (id)
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByInstrId(int instrid);
+
+        // get fitness classes for a specific studio (id)
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByStudio(string studname);
+
+    }
+}
---
+++ GymRepository/RealDB.cs
@@ -0,0 +1,92 @@
+﻿using GymModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymRepository
+{
+    public class RealDB: IGymRepo
+    {
+
+
+        /*********************** Fitness Instructor ***********************/
+        public void AddInstructor(FitnessInstructor fitinstr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessInstructor> GetInstructors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessInstructor> GetAllInstructorsbyName(string instrname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FitnessInstructor GetInstructorbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************** Fitness Studio ***********************/
+        public void AddStudio(FitnessStudio fitstudio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessStudio> GetStudios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FitnessStudio GetStudiobyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessStudio> GetStudiosbyName(string sutdname)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*********************** Fitness Classes ***********************/
+        public void AddFitClass(FitnessClassSchedule fitclass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditFitClass(int id, FitnessClassSchedule fitclass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFitClass(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessClassSchedule> GetFitClassSchedules()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByDay(DayOfWeek day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByInstrId(int instrid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FitnessClassSchedule> GetFitClassScheduleByStudio(string studname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
